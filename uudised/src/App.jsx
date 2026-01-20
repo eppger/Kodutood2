fe109a1d36405b90bc776df581115add3ac804d9
eppger <eppsasi@gmail.com>
2026-01-20 12:06:55 +0200

    uudiste portaal 1 osa

--- uudised/src/App.jsx
+++ uudised/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { Link, Routes } from'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Link to="/">
+        <button>Avalehele</button>
+      </Link>
+      <Link to="/uudised">
+        <button>/Uudiste lehele</button>
+      </Link>
+      <Link to="/kontakt">
+        <button>Võta meiega ühendust</button>
+      </Link>
+      <Link to="/meist">
+        <button>Info meist</button>
+      </Link>
+
+     <Routes>
+      <Routes path='' element={ <div>See on avaleht, nähtav localhost:3000 aadressil </div>} />
+      <Routes path='uudised' element={ <div>See on uudiste leht, nähtav localhost:3000 aadressil </div>} />
+      <Routes path='kontakt' element={ <div>See on kontaktide leht, nähtav localhost:3000 aadressil </div>} />
+      <Routes path='meist' element={ <div>See on meist leht, nähtav localhost:3000 aadressil </div>} />
+     </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
